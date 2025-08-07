ef51a6fd990bfa7671bf800faf69d316960cfefe
Akash Rai <akashrai932106@gmail.com>
2025-08-07 12:00:11 +0530

    first commit

---
+++ LWC_DATATABLE/force-app/main/default/lwc/datatableDemo/datatableDemo.js
@@ -0,0 +1,41 @@
+import { LightningElement, wire } from 'lwc';
+import fetchContact from '@salesforce/apex/ContactController.fetchContact';
+
+const columns = [
+    { label: 'Contact Name', fieldName: 'Name' },
+    {
+        label: 'accountLink', fieldName: 'accountLink', type: 'url',
+        typeAttributes: {
+            label: {
+                fieldName: 'accountName'
+            },
+            target: "_blank"
+        }
+    },
+    { label: 'Phone', fieldName: 'Phone', type: 'phone' },
+    { label: 'Email', fieldName: 'Email', type: 'email' }
+]
+
+export default class DatatableDemo extends LightningElement {
+
+    contacts;
+    column = columns;
+    @wire(fetchContact)
+    contactdata({ data, error }) {
+        if (data) {
+            //console.log('data : ', data);
+            this.contacts = data.map(record => {
+                let accountLink = "/" + record.AccountId;
+                let accountName = record.Account.Name;
+                return {
+                    ...record,
+                    accountLink: accountLink,
+                    accountName: accountName
+                }
+            })
+
+        } else if (error) {
+            console.error('error : ', error);
+        }
+    }
+}
---
+++ LWC_DATATABLE/force-app/main/default/lwc/datatableRelatedContactDemo/datatableRelatedContactDemo.js
@@ -0,0 +1,62 @@
+import { api, LightningElement, wire } from 'lwc';
+import fetchRelatedContactAccount from '@salesforce/apex/ContactController.fetchRelatedContactAccount';
+import { ShowToastEvent } from 'lightning/platformShowToastEvent';
+import { updateRecord } from 'lightning/uiRecordApi';
+const columns = [
+    { label: "First name", fieldName: "FirstName", editable: true },
+    { label: "Last Name", fieldName: "LastName", editable: true },
+    { label: "Phone", fieldName: "Phone", type: "Phone" },
+    { label: "Email", fieldName: "Email", type: "url" }
+]
+export default class DatatableRelatedContactDemo extends LightningElement {
+
+    @api recordId;
+    columns = columns;
+    contactListData = [];
+    draftValue;
+
+    @wire(fetchRelatedContactAccount,
+        {
+            accountId: "$recordId"
+        }
+    )
+    contactData({ data, error }) {
+        if (data) {
+            this.contactListData = data;
+            //console.log('data : ', data);
+        } else if (error) {
+            console.error('error : ', error);
+        }
+    }
+
+    async saveHandler(event) {
+        this.draftValue = event.detail.draftValues;
+        let updateRecordArray = this.draftValue.map(curritem => {
+            let fv = { ...curritem };
+            return {
+                fields: fv
+            }
+        });
+        this.draftValue = [];
+        updateRecordArray.map(curritem => {
+            updateRecord(curritem).then(() => {
+                this.dispatchEvent(
+                    new ShowToastEvent({
+                        title: "records updated",
+                        message: "updated successfully",
+                        variant: "success"
+                    })
+                )
+            }).catch(error => {
+                this.dispatchEvent(
+                    new ShowToastEvent({
+                        title: "records error",
+                        message: error.body.message,
+                        variant: "error"
+                    })
+                )
+            })
+        });
+    }
+
+}
