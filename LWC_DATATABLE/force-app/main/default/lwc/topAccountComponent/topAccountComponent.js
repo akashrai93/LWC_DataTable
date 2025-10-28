8e616cf5e7fa5516723872c0cc3cb8898b21be2d
Akash Rai <akashrai932106@gmail.com>
2025-10-28 11:08:51 +0100

    account is clickable now

--- LWC_DATATABLE/force-app/main/default/lwc/topAccountComponent/topAccountComponent.js
+++ LWC_DATATABLE/force-app/main/default/lwc/topAccountComponent/topAccountComponent.js
@@ -1,7 +1,15 @@
 import { LightningElement, wire } from 'lwc';
 import fetchTopAccount from '@salesforce/apex/ApexToLWC.fetchTopAccount';
 const columns = [
-    { label: 'Name', fieldName: 'Name' },
+    {
+        label: 'Name', fieldName: 'accountUrl', type: 'url',
+        typeAttributes: {
+            label: {
+                fieldName: 'Name'
+            },
+            target: "_blank"
+        }
+    },
     { label: 'Industry', fieldName: 'Industry' },
     { label: 'Rating', fieldName: 'Rating' },
     { label: 'Phone', fieldName: 'Phone', type: 'phone' }
@@ -15,8 +23,10 @@ export default class TopAccountComponent extends LightningElement {
     @wire(fetchTopAccount)
     getAccountRecords({ error, data }) {
         if (data) {
-            this.data = data;
-            console.log('data : ', data);
+            this.data = data.map(record => ({
+                ...record,
+                accountUrl: '/' + record.Id
+            }))
         } else if (error) {
             console.log(error.message.body);
         }
